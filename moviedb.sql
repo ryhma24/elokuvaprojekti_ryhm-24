90a668635f658f18ae12f122475a005d7ba1ede1
TomPohjola <tom1996666@gmail.com>
2025-11-10 12:23:04 +0200

    Update moviedb.sql

--- moviedb.sql
+++ moviedb.sql
@@ -2,7 +2,7 @@ create table account (
  idaccount serial primary key,
  username varchar(45) not null,
  email varchar(45) not null,
- password varchar (45) not null,
+ password varchar (60) not null,
  refresh_token varchar(100)
 );
 
@@ -10,14 +10,14 @@ create table groups (
  idgroup serial primary key, 
  group_color varchar(45),
  movie_list int
- CONSTRAINT fk_idaccount REFERENCES account(idaccount)
+ user_idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount);
 );
 
 create table movie_data (
  idmovie_data serial primary key, 
  reviews varchar(200),
  favourites int
- CONSTRAINT fk_idaccount REFERENCES account(idaccount)
+ user_idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount);
 );
 
 
