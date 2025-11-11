515893885af1df5807e2a388167a86d77ef0022f
Eerik Pitkänen <eerikpit@gmail.com>
2025-11-11 12:10:52 +0200

    Updated sql and ERD

--- moviedb.sql
+++ moviedb.sql
@@ -7,17 +7,23 @@ create table account (
 );
 
 create table groups (
- idgroup serial primary key, 
+ idgroup serial primary key,
+ name varchar(20),
  group_color varchar(45),
- movie_list int
- user_idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount);
+ member_status boolean,
+ movie_list int,
+ owner INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount)
 );
 
-create table movie_data (
- idmovie_data serial primary key, 
- reviews varchar(200),
- favourites int
- user_idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount);
+create table favourites (
+ idfavourites serial primary key,
+ favourites int,
+ idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount)
 );
 
-
+create table reviews (
+ idreviews serial primary key,
+ review varchar(200),
+ rating float,
+ idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount)
+);
