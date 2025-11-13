b4ac7c146226ba2f90a723ba5a80f786c37037c9
TomPohjola <tom1996666@gmail.com>
2025-11-14 00:31:27 +0200

    Update moviedb.sql

--- moviedb.sql
+++ moviedb.sql
@@ -3,7 +3,7 @@ create table account (
  username varchar(45) not null,
  email varchar(45) not null,
  password varchar (60) not null,
- refresh_token varchar(100)
+ refresh_token varchar(200)
 );
 
 create table groups (
@@ -12,19 +12,21 @@ create table groups (
  group_color varchar(45),
  member_status boolean,
  movie_list int,
- owner INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount)
+ owner INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount) ON DELETE CASCADE
 );
 
 create table favourites (
  idfavourites serial primary key,
  favourites int,
- idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount)
+ idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount) ON DELETE CASCADE
 );
 
 create table reviews (
  idreviews serial primary key,
  review varchar(200),
  rating float,
- idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount),
+ idaccount INT NOT NULL CONSTRAINT foreign_idaccount REFERENCES account(idaccount) ON DELETE CASCADE,
  idmovie int
+ date DATE
 );
+
