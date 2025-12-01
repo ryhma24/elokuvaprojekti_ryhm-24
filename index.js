be77923425f8b1ffbaa209af615ea2328cae0dd0
Eerik <epitkane19@student.oulu.fi>
2025-12-01 14:07:06 +0200

    merge

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 
 //app.use(cors())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5174",
   credentials: true // Allow cookies
 }));
 app.use(express.json());
