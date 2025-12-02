ebc93a2a87b191bbdbcc14b2c8368197be80644c
TomPohjola <tom1996666@gmail.com>
2025-12-02 20:06:48 +0200

    tom branch

--- api src/routers/account_router.js
+++ api src/routers/account_router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAccounts, addAccount, login, refreshAccessToken, logout, deleteOneAccount, setAccountForDeletion, cancelAccountDeletion, getFlags, getDeletionDateFromAcc } from "../controllers/account_controller.js";
+import { getAccounts, addAccount, login, refreshAccessToken, logout, deleteOneAccount, setAccountForDeletion, cancelAccountDeletion, getFlags, getDeletionDateFromAcc, changePassword } from "../controllers/account_controller.js";
 import { authenticateToken } from "../middleware/auth.js";
 
 const accountRouter = Router();
@@ -10,12 +10,13 @@ accountRouter.post("/login", login);
 accountRouter.post("/logout", logout);
 accountRouter.post("/refresh", refreshAccessToken);
 
-// Suojatut reitit (vaativat autentikoinnin)
+// Suojatut reitit (vaativat autentikoinnin) changePassword
 accountRouter.get("/getflagged/:username",authenticateToken, getFlags);
 accountRouter.get("/getdeletiondate/:username", authenticateToken, getDeletionDateFromAcc);
 accountRouter.get("/getaccounts", authenticateToken, getAccounts);
 accountRouter.delete("/deleteaccount/:id", authenticateToken, deleteOneAccount);
 accountRouter.put("/setDeletionFlag", authenticateToken, setAccountForDeletion); //vie tää suojatuksi myöhemmin
 accountRouter.put("/cancelDeletionFlag", authenticateToken, cancelAccountDeletion);
+accountRouter.put("/changePassword", authenticateToken, changePassword);
 
 export default accountRouter;
