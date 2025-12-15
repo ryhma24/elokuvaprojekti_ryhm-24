087afaac5cf244defd845209adb4cdc255cc6ec8
TomPohjola <tom1996666@gmail.com>
2025-12-15 18:06:06 +0200

    merge

--- frontend src/src/contexts/ReviewContext.jsx
+++ frontend src/src/contexts/ReviewContext.jsx
@@ -4,13 +4,16 @@ import { useAuth } from "./AuthContext";
 const ReviewContext = createContext();
 
 export const ReviewProvider = ({ children }) => {
-  const { accessToken, idaccount } = useAuth();
+  const { accessToken, idaccount, user } = useAuth();
   const [reviewState, setReviewState] = useState([]);
 
   useEffect(() => {
     async function fetchReview() {
       if (!accessToken || !idaccount) return;
 
+      console.log(accessToken)
+      console.log(idaccount)
+      console.log(user)
       try {
         const res = await fetch(`http://localhost:3001/reviews/${idaccount}`, {
           headers: {
