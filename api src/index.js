b9ee14687cc2bad96063b82d00950cebc4c35495
TimoMoilanen <timo.mo04@hotmail.com>
2025-12-16 17:38:09 +0200

    ssl fix

--- api src/index.js
+++ api src/index.js
@@ -18,7 +18,7 @@ const host = process.env.DB_HOST
 
 //app.use(cors())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "http://localhost:5173",
   credentials: true // Allow cookies
 }));
 app.use(express.json());
